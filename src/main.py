2e833e91e0ec08970a9de132ebb73f2108150eb8
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-01-08 15:56:32 +0900

    Changed: main src

--- src/main.py
+++ src/main.py
@@ -1,14 +1,46 @@
 # -*- coding: utf-8 -*-
 """Main story.
 """
+## path setting
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append('storybuilder')
+## public libs
+## local libs
 from storybuilder.builder.world import World
+from storybuilder.builder.writer import Writer
+## assets
 from storybuilder.assets import basic
+## local files
 from config import DAYS, ITEMS, LAYERS, PERSONS, RUBIS, STAGES, TIMES, WORDS
 from src.demo.main import ep_demo
+
+## define alias
+W = Writer
+_ = Writer.getWho()
+
+################################################################
+#
+# Sample step:
+# 1) Create the world
+#       世界を作成する。
+# 2) Create a new chapter
+#       章の作成。
+# 3) Create a episode
+#       エピソード作成。
+# 4) Create a new scene
+#       シーン作成。物語のベース。ここに様々なActionを追加する。
+# 5) Create a new stage
+#       舞台作成。シーンに必須要素
+# 6) Create a new day and time
+#       日時作成。シーンのサブ要素
+# 7) Add a scene plot
+#       シーンプロットの作成。概要のないシーンは原則使えない
+# 8) Add scene actions
+#       シーンアクションの追加。
+#
+################################################################
 
 
 ## main
@@ -17,7 +49,7 @@ def ch_main(w: World):
             ep_demo(w),
             )
 
-def world():
+def create_world():
     """Create a world.
     """
     w = World("title")
@@ -26,13 +58,16 @@ def world():
     w.buildDB(PERSONS,
             STAGES, ITEMS, DAYS, TIMES, WORDS,
             RUBIS, LAYERS)
-    w.entryBlock(
-            )
+    # w.setBaseDate()
+    # set textures
+    # w.entryBlock()
+    # w.entryHistory()
+    # w.entryLifeNote()
     return w
 
 
 def main(): # pragma: no cover
-    w = world()
+    w = create_world()
     return w.build(
             ch_main(w),
             )
